4d99c81ee4ae7dbd6f27270af4243dbc857633a9
beyescay <beyescay@gmail.com>
2018-04-02 19:21:23 -0500

    Put in functions

--- review.py
+++ review.py
@@ -14,91 +14,47 @@ dcap["phantomjs.page.settings.userAgent"] = (ua.random)
 service_args=['--ssl-protocol=any','--ignore-ssl-errors=true']
 driver = webdriver.Chrome('/home/sarath/Documents/Shiva/shiva_040218/chromedriver',desired_capabilities=dcap,service_args=service_args)
 
-#access website
 driver.get('http://www.yelp.com')
 
-#find and click login icon
+def login(username, password_value):
+
+    print("Finding log in button")
+    icon=driver.find_element_by_id('header-log-in')
+    icon.click()
+
+    print("Filling email")
+    wait = WebDriverWait(driver, 10)
+    email = driver.find_elements_by_name("email")
+
+    for element in email:
+        try:
+            element.send_keys(username)
+        except:
+            pass
+
+    password=driver.find_elements_by_name("password")
+
+    for element in password:
+        try:
+            element.send_keys(password_value)
+            element.send_keys(Keys.TAB)
+            element.send_keys(Keys.RETURN)
+        except:
+            pass
 
 
-print("Finding log in button")
-icon=driver.find_element_by_id('header-log-in')
-icon.click()
-
-#print("Switching to pop up")
-#find and switch to the popup fram
-
-print("Filling email")
-
-wait = WebDriverWait(driver, 10)
-
-email=driver.find_elements_by_name("email")
-for element in email:
-    try:
-        element.send_keys('shivahw040218@gmail.com')
-    except:
-        pass
-
-password=driver.find_elements_by_name("password")
-for element in password:
-    try:
-        element.send_keys('ilovecoding')
-        element.send_keys(Keys.TAB)
-        element.send_keys(Keys.RETURN)
-    except:
-        pass
-
-
-"""
-login=driver.find_elements_by_class_name('ybtn ybtn--primary ybtn--big submit ybtn-full')
-for button in login:
-    try:
-        button.click()
-    except:
-        pass
-"""
-
-"""
-
-
-
-print("Filling password")
-#find and fill the password box
-pswd=WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'password')))
-pswd.send_keys('ilovecoding')
-
-print("Click log in")
-#find and click the login button
-button=WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'ybtn ybtn--primary ybtn--big submit ybtn-full')))
-button.click()
-
-def login():
+def submitReview(rev_text, rev_rating, restaurantID):
     pass
 
 
-#find and click the Restaurants button
-myElem = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'global-nav-restaurants')))
-
-el=WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'global-nav-restaurants')))
-el.click()
-
-#find and fill the search box
-#searchBox=driver.find_element_by_class_name('typeahead_input')
-searchBox=WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'typeahead_input')))
-searchBox.send_keys('Hoboken')
-
-#find and click the search button
-button=WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'SUBMIT_RESTAURANTS')))
-button.click()
-
-#wait until the page loads
-myElem = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME,'property_title')))
-
-#find all the restaurants in the first page and print their names
-els=driver.find_elements_by_class_name('property_title')
-for el in els:
-    print (el.text)
-"""
+def vote(userID):
+    pass
 
 
-if __name__ == "__main":
-    login()
+def test(username,password,rev_text, rev_rating, restaurantID, userID):
+    pass
+
+
+if __name__ == "__main__":
+    login('shivahw040218@gmail.com', 'ilovecoding')
+    #submitReview()
